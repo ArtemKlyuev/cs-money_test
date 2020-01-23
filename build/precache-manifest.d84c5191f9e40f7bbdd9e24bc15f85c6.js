3dc87e8fe1eb6ba15e269715f3389a735c168fe6
Artem Klyuev <klyuev@sizmek.ru>
2020-01-23 19:12:47 +0300

    upd

---
+++ build/precache-manifest.d84c5191f9e40f7bbdd9e24bc15f85c6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ecb11b2d671a5cbe45842de2dbc81a46",
+    "url": "/cs-money_test/index.html"
+  },
+  {
+    "revision": "d0b406b58f1425853af7",
+    "url": "/cs-money_test/static/css/main.150af197.chunk.css"
+  },
+  {
+    "revision": "8fbe1f0dfc440363b991",
+    "url": "/cs-money_test/static/js/2.0410c986.chunk.js"
+  },
+  {
+    "revision": "efe80d506e138c343a03a1541511a2f3",
+    "url": "/cs-money_test/static/js/2.0410c986.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d0b406b58f1425853af7",
+    "url": "/cs-money_test/static/js/main.b75c702a.chunk.js"
+  },
+  {
+    "revision": "4160da7a16abfa824be9",
+    "url": "/cs-money_test/static/js/runtime-main.1850be5c.js"
+  }
+]);
